55fdb09a8e5297a62e9e43c2e61b56faf821d74e
xujingchen1996 <41528932+xujingchen1996@users.noreply.github.com>
2019-07-04 12:11:11 +0800

    set timezone and language

--- CloudPocket/settings.py
+++ CloudPocket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sg7mcryjcn9a+7ixy1ee^x&=qskw7#(m+=ut_vh#h_ce_&f^px'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asian/Shanghai'
 
 USE_I18N = True
 
